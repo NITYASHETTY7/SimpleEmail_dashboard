8c51198bbc7598e3e0249e23b4ce43fd025a3bd2
Nitya Shetty <nityashetty21@gmail.com>
2026-08-21 23:30:10 +0530

    Import useEffect and api in EmailDetailModal

--- frontend/src/components/common/EmailDetailModal.tsx
+++ frontend/src/components/common/EmailDetailModal.tsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { EmailJob } from '../../types';
+import { api } from '../../services/api';
 import {
   X,
   Clock,
